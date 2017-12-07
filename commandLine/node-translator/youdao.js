3ffa430e0829c34c88be8967f6276b07cb4b943e
Thomson <779387850@qq.com>
2017-12-07 10:42:53 +0800

    update

--- commandLine/node-translator/youdao.js
+++ commandLine/node-translator/youdao.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const request = require('request')
-const colors = require('colors')
 let param = process.argv[2]
 let word = param ? param : ''
 request('http://fanyi.youdao.com/openapi.do?keyfrom=node-translator&key=2058911035&type=data&doctype=json&version=1.1&q=' + word, function (error, response, body) {
