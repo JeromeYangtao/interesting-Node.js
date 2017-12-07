9094957054e1dc2d329586a4dac3afeb10d45de4
Thomson <779387850@qq.com>
2017-12-07 10:38:46 +0800

    格式化翻译后的结果

--- commandLine/node-translator/youdao.js
+++ commandLine/node-translator/youdao.js
@@ -6,6 +6,8 @@ let param = process.argv[2]
 let word = param ? param : ''
 request('http://fanyi.youdao.com/openapi.do?keyfrom=node-translator&key=2058911035&type=data&doctype=json&version=1.1&q=' + word, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    console.log(body);
+    JSON.parse(body).basic.explains.forEach((para) => {
+      console.log(para)
+    })
   }
-});
+})
