d91224bf26267d8939a6e9f6e9697bc7b71c69ac
Thomson <779387850@qq.com>
2017-12-15 14:22:17 +0800

    检索HTML里面的link标签并替换

--- commandLine/px2rem/px2rem.js
+++ commandLine/px2rem/px2rem.js
@@ -9,7 +9,6 @@ let isHTML = !!filename.match(/.html\b/)
 
 try {
   let data = fs.readFileSync(filePath, 'utf-8')
-
   if (isCSS && !isHTML) {
     // 替换css文件
     let newData = data.replace(/\d+px\b/g, (match) => {
@@ -19,13 +18,27 @@ try {
     })
     fs.writeFileSync(filePath, newData)
   } else if (isHTML && !isCSS) {
-    // 替换HTML文件
+    // 替换HTML文件的内联样式
     let newData = data.replace(/\d+px/g, (match) => {
       let pxNum = match.slice(0, match.length - 2)
       let remNum = convert(pxNum)
       return `${remNum}rem`
     })
+
+    //检索HTML引用的外部样式并替换
+    //先考虑匹配一个的情况
+    let linkTag = data.match(/\<link.*\>/)[0]
+    let cssHref = linkTag.match(/href=".*"/)[0].slice(6,-1)
+    console.log(cssHref)
+    let cssData = fs.readFileSync(cssHref, 'utf-8')
+    let newCssData = cssData.replace(/\d+px/g, (match) => {
+      let pxNum = match.slice(0, match.length - 2)
+      let remNum = convert(pxNum)
+      return `${remNum}rem`
+    })
     console.log(newData)
+    console.log(newCssData)
+    // fs.writeFileSync(cssHref,newCssData)
     // fs.writeFileSync(filePath, newData)
   } else {
     console.log('文件类型错误')
@@ -35,9 +48,11 @@ try {
   console.log('路径参数错误')
 }
 
+/*
+*px数转化为rem数
+* 默认1rem=20px
+* */
 function convert (pxNum) {
-  // px数转化为rem数
-  //默认1rem=20px
   let ratio = process.argv[3] ? process.argv[3] : 20
   console.log(`px:rem=${ratio}`)
   return pxNum / ratio
