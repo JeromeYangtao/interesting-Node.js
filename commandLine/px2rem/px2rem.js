3019714f619e345f00cb7b45bba0ab9659888e2d
Thomson <779387850@qq.com>
2017-12-15 15:07:37 +0800

    dry原则重构

--- commandLine/px2rem/px2rem.js
+++ commandLine/px2rem/px2rem.js
@@ -11,51 +11,25 @@ try {
   let data = fs.readFileSync(filePath, 'utf-8')
   if (isCSS && !isHTML) {
     // 替换css文件
-    let newData = data.replace(/\d+px\b/g, (match) => {
-      let pxNum = match.slice(0, match.length - 2)
-      let remNum = convert(pxNum)
-      return `${remNum}rem`
-    })
-    fs.writeFileSync(filePath, newData)
+    let newData = getReplacedData(data)
+    console.log(newData)
+    // fs.writeFileSync(filePath, newData)
   } else if (isHTML && !isCSS) {
     // 替换HTML文件的内联样式
-    let newData = data.replace(/\d+px/g, (match) => {
-      let pxNum = match.slice(0, match.length - 2)
-      let remNum = convert(pxNum)
-      return `${remNum}rem`
-    })
-
+    let newData = getReplacedData(data)
     //检索HTML引用的外部样式并替换
-    //先考虑匹配一个的情况
     data.match(/\<link.*\>/g).forEach((x) => {
       let cssHref = x.match(/href=".*"/)[0].slice(6, -1)
       let cssData = fs.readFileSync(cssHref, 'utf-8')
-      let newCssData = cssData.replace(/\d+px/g, (match) => {
-        let pxNum = match.slice(0, match.length - 2)
-        let remNum = convert(pxNum)
-        return `${remNum}rem`
-      })
-      console.log(cssHref)
+      let newCssData = getReplacedData(cssData)
       console.log(newCssData)
       // fs.writeFileSync(cssHref,newCssData)
-      return
     })
-
-    // let cssHref = linkTag.match(/href=".*"/)[0].slice(6,-1)
-    // console.log(cssHref)
-    // let cssData = fs.readFileSync(cssHref, 'utf-8')
-    // let newCssData = cssData.replace(/\d+px/g, (match) => {
-    //   let pxNum = match.slice(0, match.length - 2)
-    //   let remNum = convert(pxNum)
-    //   return `${remNum}rem`
-    // })
-
     console.log(newData)
     // fs.writeFileSync(filePath, newData)
   } else {
     console.log('文件类型错误')
   }
-
 } catch (error) {
   console.log('路径参数错误')
 }
@@ -67,6 +41,17 @@ try {
 function convert (pxNum) {
   let ratio = process.argv[3] ? process.argv[3] : 20
   return pxNum / ratio
+}
+
+/*
+*把文本内的px转化为rem
+* */
+function getReplacedData (data) {
+  return data.replace(/\d+px/g, (match) => {
+    let pxNum = match.slice(0, match.length - 2)
+    let remNum = convert(pxNum)
+    return `${remNum}rem`
+  })
 }
 
 // px2rem index.css 20
